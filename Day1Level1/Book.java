a6bdf7265fe395d490b17a34ce8884082a582cee
Anshul Rai <anshulrai02092003@gmail.com>
2025-01-16 15:17:54 +0530

    Represent book details such as title, author, and ISBN.

---
+++ Day1Level1/Book.java
@@ -0,0 +1,24 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    // Constructor to initialize book details
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 399.99);
+        book.displayDetails();
+    }
+}

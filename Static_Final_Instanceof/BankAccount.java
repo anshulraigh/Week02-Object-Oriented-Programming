04dd06849466549561a454c7185c97153bb5198c
Anshul Rai <anshulrai02092003@gmail.com>
2025-01-20 16:14:26 +0530

    Introduced static methods and variables to enhance functionality and manage shared resources efficiently.

--- Static_Final_Instanceof/BankAccount.java
+++ Static_Final_Instanceof/BankAccount.java
@@ -1,14 +1,14 @@
-class BankAccount {
+ class BankAccounts {
     // Static variable shared by all accounts
-    static String bankName = "Global Bank"; 
+    static String bankName = "SBI Bank"; 
     private static int totalAccounts = 0; // Tracks the total number of accounts
 
     // Final variable ensures account number cannot be changed
     private final int accountNumber; 
     private String accountHolderName;
 
-    // Constructor to initialize account details using 'this' keyword
-    public BankAccount(String accountHolderName, int accountNumber) {
+    // Constructor to initialize account details  
+      BankAccounts(String accountHolderName, int accountNumber) {
         this.accountHolderName = accountHolderName;
         this.accountNumber = accountNumber;
         totalAccounts++; // Increment total accounts count
@@ -21,24 +21,25 @@ class BankAccount {
 
     // Display account details, checking if the object is an instance of BankAccount
     void displayDetails() {
-        if (this instanceof BankAccount) {
+        if (this instanceof BankAccounts) {
             System.out.println("Account Holder: " + accountHolderName);
             System.out.println("Account Number: " + accountNumber);
         }
     }
 }
 
-public class Main {
+public class BankAccount{
     public static void main(String[] args) {
         // Creating BankAccount objects
-        BankAccount acc1 = new BankAccount("Alice", 101);
-        BankAccount acc2 = new BankAccount("Bob", 102);
+        BankAccounts acc1 = new BankAccounts("DEEPAK", 101);
+        BankAccounts acc2 = new BankAccounts("CHOURSIYA", 102);
 
         // Displaying account details
         acc1.displayDetails();
         acc2.displayDetails();
 
         // Display total accounts
-        System.out.println("Total Accounts: " + BankAccount.getTotalAccounts());
+        System.out.println("Total Accounts: " + BankAccounts.getTotalAccounts());
     }
 }
+
